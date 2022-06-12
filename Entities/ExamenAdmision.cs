66fda30d79e0157cbe13775cb01199959a2530d1
alvaro florez flores perez <alvaritotutos@gmail.com>
2022-06-12 10:40:48 -0600

    update examen admision

--- Entities/ExamenAdmision.cs
+++ Entities/ExamenAdmision.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiKalum_Backend.Entities
 {
     public class ExamenAdmision
     {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string ExamenId { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public DateTime FechaExamen { get; set; }
-
         public virtual List<Aspirante> Aspirantes { get; set; }
     }
 }
