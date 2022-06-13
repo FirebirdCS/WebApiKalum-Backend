83eb0aef52ff81d1584d847b02976985d5088fb5
alvaro florez flores perez <alvaritotutos@gmail.com>
2022-06-13 16:44:01 -0600

    Update aspirante

--- Entities/Aspirante.cs
+++ Entities/Aspirante.cs
@@ -1,15 +1,23 @@
+using System.ComponentModel.DataAnnotations;
 using WebApiKalum.Entities;
 
 namespace WebApiKalum_Backend.Entities
 {
     public class Aspirante
     {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string NoExpediente { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Apellidos { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Nombres { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Direccion { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Estatus { get; set; }
         public string CarreraId { get; set; }
         public string JornadaId { get; set; }
