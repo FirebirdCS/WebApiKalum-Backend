a1765385f5b1554f8b0f22c3b1c2a7ec63617579
FirebirdCS <alvaritotutos@gmail.com>
2022-06-27 18:37:53 -0600

    Update resultado de examen admision

---
+++ Controllers/ResultadoExamenAdmisionController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiKalum_Backend.Controllers
+{
+    [ApiController]
+    [Route("v1/KalumManagement/ResultadosExamenAdmision")]
+    public class ResultadoExamenAdmisionController : ControllerBase
+    {
+
+    }
+}
